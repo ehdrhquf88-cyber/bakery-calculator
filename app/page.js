ee65b4011bb423af3731c19a2321624a35df0e5b
ehdrhquf88-cyber <ehdrhquf88@gmail.com>
2026-08-12 19:04:37 +0200

    로딩 페이지 수정

--- app/page.js
+++ app/page.js
@@ -1063,8 +1063,8 @@ function LoadingIdentity({ t }) {
                 d="M80,80 m-56,0 a56,56 0 1,1 112,0 a56,56 0 1,1 -112,0"
               />
             </defs>
-            <text className="fill-black text-[12px] font-black tracking-[0.08em]">
-              <textPath href="#loadingIdentityCircle" startOffset="0%" textLength="352" lengthAdjust="spacing">
+            <text className="fill-black text-[10px] font-black tracking-[0.06em]">
+              <textPath href="#loadingIdentityCircle" startOffset="0%">
                 {t("loadingIdentityText")}
               </textPath>
             </text>
